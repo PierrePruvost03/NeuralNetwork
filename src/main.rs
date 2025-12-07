41669bdf077d02b46f2636d1fa777f0c00b6117e
abeldaverio <abel.daverio@epitech.eu>
2025-12-07 11:22:21 +0100

    feat(datastruct): perceptron

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,45 @@
-use rand::Rng;
+// use rand::Rng;
 
-pub struct Perceptron {
-    func: fn(&Perceptron, f64) -> bool,
-    weigth: Vec<f64>,
-    learing_rate: f64,
-    biais: f64,
-}
+// pub struct Perceptron {
+//     func: fn(&Perceptron, f64) -> bool,
+//     weigth: Vec<f64>,
+//     learing_rate: f64,
+//     biais: f64,
+// }
 
-impl Perceptron {
-    fn exec(&self, args: Vec<f64>) -> bool {
-        (self.func)(self, args.iter().sum())
-    }
-}
+// impl Perceptron {
+//     fn exec(&self, args: Vec<f64>) -> bool {
+//         (self.func)(self, args.iter().sum())
+//     }
+// }
 
-impl Perceptron {
-    pub fn new(base_weigths: Vec<f64>, learning_rate: f64, biais: f64) -> Self {
-        Perceptron {
-            func: |per, sum| (sum * per.learing_rate) >= 0.,
-            weigth: base_weigths,
-            learing_rate: learning_rate,
-            biais: biais,
-        }
-    }
+// impl Perceptron {
+//     pub fn new(base_weigths: Vec<f64>, learning_rate: f64, biais: f64) -> Self {
+//         Perceptron {
+//             func: |per, sum| (sum * per.learing_rate) >= 0.,
+//             weigth: base_weigths,
+//             learing_rate: learning_rate,
+//             biais: biais,
+//         }
+//     }
 
-    pub fn new_r(nb_weigth: u32, learning_rate: f64, biais: f64) -> Self {
-        Perceptron {
-            func: |per, sum| (sum * per.learing_rate) >= 0.,
-            weigth: (0..nb_weigth)
-                .map(|_| rand::random_range(0.0..1.))
-                .collect(),
-            learing_rate: learning_rate,
-            biais: biais,
-        }
-    }
-}
+//     pub fn new_r(nb_weigth: u32, learning_rate: f64, biais: f64) -> Self {
+//         Perceptron {
+//             func: |per, sum| (sum * per.learing_rate) >= 0.,
+//             weigth: (0..nb_weigth)
+//                 .map(|_| rand::random_range(0.0..1.))
+//                 .collect(),
+//             learing_rate: learning_rate,
+//             biais: biais,
+//         }
+//     }
+// }
+
+mod network;
+use crate::network::datastruct::perceptron::Perceptron;
 
 fn main() {
-    let les_pieds = Perceptron::new_r(2, 23.0, 3.);
-
-    println!("{}", les_pieds.exec(vec![0., -1.]));
+    let les_pieds = Perceptron::new(String::from("sigmoid 0. 1 2")).unwrap();
+    println!("{:?}", les_pieds.exec(vec![0., 0.]));
+    println!("{}", les_pieds.to_string());
 }
---
+++ src/network/datastruct/perceptron.rs
@@ -0,0 +1,53 @@
+use rand::distr::uniform::Error;
+use std::str::FromStr;
+
+pub fn function_getter(key: String) -> Result<fn(f64, f64) -> f64, String> {
+    match key.as_str() {
+        "sigmoid" => Ok(|i, b| 1. / (1. + (i + b).exp())),
+        _ => Err(format!("unknow function '{}'", key)),
+    }
+}
+
+pub struct Perceptron {
+    pub func: fn(f64 /* Input sum */, f64 /* Bias */) -> f64,
+    pub func_id: String,
+    pub weights: Vec<f64>,
+    pub biais: f64,
+}
+
+impl Perceptron {
+    pub fn new(config: String) -> Result<Self, String> {
+        let mut tokens = config.split_whitespace();
+        let f_id = String::from(tokens.next().unwrap());
+        let f = function_getter(f_id.clone())?;
+        let b: f64 = tokens.next().unwrap().parse().unwrap();
+        let w: Vec<f64> = tokens.map(f64::from_str).map(Result::unwrap).collect();
+        Ok(Perceptron {
+            func: f,
+            func_id: f_id,
+            weights: w,
+            biais: b,
+        })
+    }
+
+    pub fn exec(&self, inputs: Vec<f64>) -> f64 {
+        if inputs.len() != self.weights.len() {
+            panic!();
+        }
+        let sum = inputs
+            .iter()
+            .zip(self.weights.iter())
+            .fold(0.0, |acc, (i, w)| acc + i * w);
+        (self.func)(sum, self.biais)
+    }
+
+    pub fn to_string(&self) -> String {
+        let weight_string = self
+            .weights
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ");
+        format!("{} {} {}", self.func_id, self.biais, weight_string)
+    }
+}
