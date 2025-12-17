d7c5a0b7e89125efd724d3392ba6129e65e87c26
PierrePruvost03 <pierre.pruvost@epitech.eu>
2025-12-17 21:34:42 +0100

    feat: main setup using parse_config to run the good methods

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,30 @@
-//
-
-// pub struct Perceptron {
-//     func: fn(&Perceptron, f64) -> bool,
-//     weigth: Vec<f64>,
-//     learing_rate: f64,
-//     biais: f64,
-// }
-
-// impl Perceptron {
-//     fn exec(&self, args: Vec<f64>) -> bool {
-//         (self.func)(self, args.iter().sum())
-//     }
-// }
-
-// impl Perceptron {
-//     pub fn new(base_weigths: Vec<f64>, learning_rate: f64, biais: f64) -> Self {
-//         Perceptron {
-//             func: |per, sum| (sum * per.learing_rate) >= 0.,
-//             weigth: base_weigths,
-//             learing_rate: learning_rate,
-//             biais: biais,
-//         }
-//     }
-
-//     pub fn new_r(nb_weigth: u32, learning_rate: f64, biais: f64) -> Self {
-//
-//     }
-// }
-
+mod chess;
 mod network;
-use crate::network::datastruct::{network::Network, perceptron::Perceptron};
+mod parse_config;
 
-// fn main() {
-//     let network = Network::new_random(3, vec![4, 3], (0., 3.4), (-1., 1.5));
-
-//     let network_config = network.to_string();
-
-//     let parsed_network = Network::new(network_config).unwrap();
-
-//     println!("{:?}", network.exec(vec![0., 0., 0.1]));
-
-//     println!("{:?}", parsed_network.exec(vec![0., 0., 0.1]));
-// }
+use parse_config::{Config, Mode};
 
 fn main() {
-    let mut network = Network::new_random(2, vec![2, 1], (-1., 1.), (-1., 1.));
-    let xor_results = vec![
-        (vec![0., 0.], vec![0.]),
-        (vec![1., 0.], vec![1.]),
-        (vec![0., 1.], vec![1.]),
-        (vec![1., 1.], vec![0.])
-    ];
-    let learning_rate = 0.5;
-    let epochs = 10000;
-
-    for _ in 0..epochs {
-        for index in 0..xor_results.len() {
-            network.train(&xor_results[index].0, &xor_results[index].1, learning_rate);
+    // Parser les arguments CLI
+    let config = match Config::parse() {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            eprintln!();
+            Config::print_help();
+            std::process::exit(84);
         }
-    }
+    };
 
-    for index in 0..xor_results.len() {
-        let output = network.exec(xor_results[index].0.clone());
-        println!("{:?} -> {:.4}", xor_results[index].0.clone(), output[0]);
+    // Exécuter selon le mode
+    let result = match config.mode {
+        Mode::Predict => chess::predictor::run_predict(&config),
+        Mode::Train => chess::trainer::run_train(&config),
+    };
+
+    // Gérer les erreurs
+    if let Err(e) = result {
+        eprintln!("Error: {}", e);
+        std::process::exit(84);
     }
 }
